a153a69220fd57d7e6b7a2c3b641d93f751e58ed
jonhealy1 <jonathan.d.healy@gmail.com>
2022-04-22 01:48:48 -0700

    update functions

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,27 @@ use tide::Request;
 use tide::prelude::*;
 
 #[derive(Debug, Deserialize)]
-struct Animal {
-    name: String,
-    legs: u16,
+struct GeoJSON {
+    geojson_type: String,
+    coordinates: String,
 }
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     let mut app = tide::new();
-    app.at("/name/:name").get(root_page);
-    app.at("/orders/shoes").post(order_shoes);
+    app.at("/name/:name").get(hello_name);
+    app.at("/add/geojson").post(add_geojson);
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
 
-async fn root_page(req: Request<()>) ->  tide::Result<String> {
+async fn hello_name(req: Request<()>) ->  tide::Result<String> {
     let name: String = req.param("name")?.to_string();
     let msg = format!("Hello, {}!", name).into();
     Ok(msg)
 }
 
-async fn order_shoes(mut req: Request<()>) -> tide::Result {
-    let Animal { name, legs } = req.body_json().await?;
-    Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
+async fn add_geojson(mut req: Request<()>) -> tide::Result {
+    let GeoJSON { geojson_type, coordinates } = req.body_json().await?;
+    Ok(format!("Type: {}, Coordiantes: {}", geojson_type, coordinates).into())
 }
